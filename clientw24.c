fcf393fb74bd4a33ecaf005819063590017be258
anjanikumarkandula <anjanikumarkandula@gmail.com>
2024-10-17 22:26:55 -0400

    test

--- clientw24.c
+++ clientw24.c
@@ -4,7 +4,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include <arpa/inet.h> // Added for inet_pton
+#include <arpa/inet.h> 
 #include <unistd.h>
 #include<fcntl.h>
 #define MAX_MESSAGE_SIZE 255
